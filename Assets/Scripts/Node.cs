7567669fd56d9f5b2b51f76695f5c032c5056ea4
Yasin Arıcı <ysnarc2001@gmail.com>
2024-07-06 21:35:43 +0300

    coding intro p2

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -6,4 +6,10 @@ public class Node : MonoBehaviour
 {
     [SerializeField] private List<Cell> _cells;
     
+    [SerializeField] private Node _topNeighborNode;
+    [SerializeField] private Node _rightNeighborNode;
+    [SerializeField] private Node _bottomNeighborNode;
+    [SerializeField] private Node _leftNeighborNode;
+    
+    
 }
