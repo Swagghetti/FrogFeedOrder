23db43d2e9960feee6a639c341307535dd0e4a38
Yasin Arıcı <ysnarc2001@gmail.com>
2024-07-07 14:32:05 +0300

    node operations 1

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -1,35 +1,70 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Cell : MonoBehaviour
 {
-    [SerializeField] private CellColor _cellColor;
-    [SerializeField] private CellType _cellType;
-    [SerializeField] private ArrowDirection _arrowDirection;
-    
-    
+    [SerializeField] private CellColor cellColor;
+    [SerializeField] private CellType cellType;
+    [SerializeField] private ArrowDirection arrowDirection;
+
+    [SerializeField] private Material blueMaterial;
+    [SerializeField] private Material greenMaterial;
+    [SerializeField] private Material yellowMaterial;
+    [SerializeField] private Material redMaterial;
+    [SerializeField] private Material purpleMaterial;
+
+    private void Start()
+    {
+        InitializeCellColor();
+        
+    }
+
+    private void InitializeCellColor()
+    {
+        switch (cellColor)
+        {
+            case CellColor.Blue:
+                this.gameObject.GetComponent<MeshRenderer>().materials[0] = blueMaterial;
+                break;
+            case CellColor.Green:
+                this.gameObject.GetComponent<MeshRenderer>().materials[0] = greenMaterial;
+                break;
+            case CellColor.Yellow:
+                this.gameObject.GetComponent<MeshRenderer>().materials[0] = yellowMaterial;
+                break;
+            case CellColor.Red:
+                this.gameObject.GetComponent<MeshRenderer>().materials[0] = redMaterial;
+                break;
+            case CellColor.Purple:
+                this.gameObject.GetComponent<MeshRenderer>().materials[0] = purpleMaterial;
+                break;
+        }
+    }
+
+
     public enum CellColor
     {
-        blue,
-        green,
-        yellow,
-        red,
-        purple
+        Blue,
+        Green,
+        Yellow,
+        Red,
+        Purple
     }
 
     public enum CellType
     {
-        grape,
-        arrow,
-        frog,
+        Grape,
+        Arrow,
+        Frog,
     }
     
     public enum ArrowDirection
     {
-        up,
-        right,
-        down,
-        left
+        Up,
+        Right,
+        Down,
+        Left
     }
 }
--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -1,15 +1,79 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Node : MonoBehaviour
 {
-    [SerializeField] private List<Cell> _cells;
+    [SerializeField] private List<Cell> cells;
     
-    [SerializeField] private Node _topNeighborNode;
-    [SerializeField] private Node _rightNeighborNode;
-    [SerializeField] private Node _bottomNeighborNode;
-    [SerializeField] private Node _leftNeighborNode;
+    [SerializeField] private Node topNeighborNode;
+    [SerializeField] private Node rightNeighborNode;
+    [SerializeField] private Node bottomNeighborNode;
+    [SerializeField] private Node leftNeighborNode;
+
+    private void Start()
+    {
+        ValidateNeighboringNodes();
+    }
+
+    private void ValidateNeighboringNodes()
+    {
+        if (topNeighborNode != null)
+        {
+            if (!topNeighborNode.IsNeighborWith(this, RelativeDirection.Down))
+            {
+                Debug.LogWarning("Node neighboring missing: " + this.gameObject.name);
+            }
+            
+        }
+        if (rightNeighborNode != null)
+        {
+            if (!rightNeighborNode.IsNeighborWith(this, RelativeDirection.Left))
+            {
+                Debug.LogWarning("Node neighboring missing: " + this.gameObject.name);
+            }
+        }
+        if (bottomNeighborNode != null)
+        {
+            if (!bottomNeighborNode.IsNeighborWith(this, RelativeDirection.Up))
+            {
+                Debug.LogWarning("Node neighboring missing: " + this.gameObject.name);
+            }
+        }
+        if (leftNeighborNode != null)
+        {
+            if (!leftNeighborNode.IsNeighborWith(this, RelativeDirection.Right))
+            {
+                Debug.LogWarning("Node neighboring missing: " + this.gameObject.name);
+            }
+        }
+    }
+
+    //Checks if this Node is neighboring with given Node in given relativePosition
+    public bool IsNeighborWith(Node node, RelativeDirection relativePosition)
+    {
+        switch (relativePosition)
+        {
+            case RelativeDirection.Up: // upper neighbor
+                return topNeighborNode == node;
+            case RelativeDirection.Right: // right neighbor
+                return rightNeighborNode == node;
+            case RelativeDirection.Down: // bottom neighbor
+                return bottomNeighborNode == node;
+            case RelativeDirection.Left: // left neighbor
+                return leftNeighborNode == node;
+        }
+
+        return false;
+    }
     
+    public enum RelativeDirection
+    {
+        Up,
+        Right,
+        Down,
+        Left
+    }
     
 }
